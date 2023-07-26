09ca52a5c9dba8050c7db28fbac18081f7a409f7
skhila <bachana.skhiladze.1@btu.edu.ge>
2023-07-26 18:56:39 +0400

    Added several classes in figures and util packages

---
+++ src/main/java/ge/tbc/testautomation/javaoop/util/HelperFunctions.java
@@ -0,0 +1,19 @@
+package ge.tbc.testautomation.javaoop.util;
+
+import ge.tbc.testautomation.javaoop.figures.Rectangle;
+
+public class HelperFunctions {
+
+    public static void compareRectangles(Rectangle r1, Rectangle r2){
+        double r1Perimeter = r1.getPerimeter();
+        double r2Perimeter = r2.getPerimeter();
+
+        if (r1Perimeter > r2Perimeter){
+            System.out.println("perimeter #1 > perimeter #2");
+        }else if(r2Perimeter > r1Perimeter){
+            System.out.println("perimeter #1 < perimeter #2");
+        }else{
+            System.out.println("perimeter #1 = perimeter #2");
+        }
+    }
+}
--- src/main/java/ge/tbc/testautomation/javaoop/util/Util.java
+++ src/main/java/ge/tbc/testautomation/javaoop/util/Util.java
@@ -1,10 +1,13 @@
 package ge.tbc.testautomation.javaoop.util;
 
 import ge.tbc.testautomation.javaoop.figures.Circle;
+import ge.tbc.testautomation.javaoop.figures.Rectangle;
 
 public class Util {
     public static String circleToString(Circle circle){
         String returnMessage = "[Circle: radius - " + circle.getRadius() + "]";
         return returnMessage;
     }
+
+
 }
