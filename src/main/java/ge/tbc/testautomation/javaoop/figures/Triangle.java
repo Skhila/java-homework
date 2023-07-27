ce6c7a882a5f045d819e7368ee0e3ddd5484f3f6
skhila <bachana.skhiladze.1@btu.edu.ge>
2023-07-27 16:51:57 +0400

    Added some classes in features and util packages

---
+++ src/main/java/ge/tbc/testautomation/javaoop/figures/Triangle.java
@@ -0,0 +1,69 @@
+package ge.tbc.testautomation.javaoop.figures;
+
+import java.util.Random;
+
+public class Triangle extends Figure{
+    private double a;
+    private double b;
+    private double c;
+    private double h;
+
+    public Triangle(){}
+    public Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.h = generateRandomHeight();
+    }
+
+//    Overridden Functions
+    @Override
+    public double getArea() {
+//        In this case B is a Base and H is a Height of a Triangle
+        return b*h/2;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a+b+c;
+    }
+
+//    Getter/Setter
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+//    Generate random Height for Triangle
+    public static double generateRandomHeight(){
+        Random random = new Random();
+        return random.nextDouble(20);
+    }
+}
