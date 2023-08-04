45452c2099474a35552fd1972db8b03845ce6dcf
skhila <bachana.skhiladze.1@btu.edu.ge>
2023-08-05 01:46:45 +0400

    Feature: Created Annotation for variable names, Created comparator for reverse sorting Rectangle objects

--- src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
+++ src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
@@ -1,32 +1,50 @@
 package ge.tbc.testautomation.javaoop.runners;
 
-import ge.tbc.testautomation.javaoop.figures.Circle;
+import ge.tbc.testautomation.annotationsAndComparators.Analyzable;
+import ge.tbc.testautomation.annotationsAndComparators.RectangleComparator;
+import ge.tbc.testautomation.annotationsAndComparators.VariableNameAnnotation;
+import ge.tbc.testautomation.annotationsAndComparators.Rectangle;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class Main {
     public static void main(String[] args){
+//        Task #6
+        System.out.println("** Check if Annotation names equal field names in Analyzable class **");
+        Field[] analyzableFields = Analyzable.class.getDeclaredFields();
+        String fieldAnnotationName;
+        for (Field field : analyzableFields){
+            fieldAnnotationName = field.getAnnotation(VariableNameAnnotation.class).name();
+            if (!fieldAnnotationName.equalsIgnoreCase(field.getName())){
+                System.out.println("VARIABLE NAME MISMATCH FOUND");
+            }
+        }
 
-//        Task: Java Abstract Classes & Interfaces
-        Circle c1 = new Circle(11);
-        Circle c2 = new Circle(-11);
+//        Task #7
+        int unusedInt = 8;
+        boolean unusedBoolean = false;
+        String unusedString = "Hello, I'm Unused String";
 
-        System.out.println("** Area/Length **");
-        System.out.printf("c1 Area: %,.3f \n", c1.getArea());
-        System.out.printf("c2 Length: %,.3f \n", c2.getLength());
+//        Task #8
+        System.out.println("** Sorting Integer ArrayList **");
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        Collections.addAll(integerArrayList, 11, 22, 33, 5, 2, 6, 7, 1, 3, 4);
+        Collections.sort(integerArrayList);
+        System.out.println("Sorted Integer Arraylist: " + integerArrayList);
 
-        System.out.println("** Package Names **");
-        c1.printPackageName();
-        c2.printPackageName();
-
-        System.out.println("** Double sized / Custom sized Circles **");
-        c1 = c1.returnDoubleSizedCircle(c1);
-        System.out.printf("Double sized c1's radius: %,.3f \n", c1.getRadius());
-
-        c2 = c2.returnCustomSizedCircle(c2, 11);
-        System.out.printf("Custom sized c2's radius(multiplied by 11): %,.3f \n", c2.getRadius());
-
-        System.out.println("** Circle validation **");
-        System.out.printf("c1 is valid: %b \n", c1.validateCircle(c1));
-        System.out.printf("c2 is valid: %b \n", c2.validateCircle(c2));
-
+//        Task #13-14
+        System.out.println("** Reverse sorting Rectangle List using custom comparator **");
+        List<Rectangle> rectangleList = new ArrayList<>();
+        Collections.addAll(rectangleList, new Rectangle(1, 2), new Rectangle(3, 4), new Rectangle(5, 6),
+                                          new Rectangle(7, 8), new Rectangle(9, 10), new Rectangle(11, 12),
+                                          new Rectangle(13,14), new Rectangle(15, 16), new Rectangle(17, 18),
+                                          new Rectangle(19,20));
+        RectangleComparator rectangleComparator = new RectangleComparator();
+        rectangleList.sort(rectangleComparator);
+        System.out.println("Reverse sorted Rectangle List: " + rectangleList);
     }
 }
