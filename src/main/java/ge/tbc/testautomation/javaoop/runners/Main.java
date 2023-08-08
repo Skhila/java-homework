322c240c0bef841e94bd8860161a0ead1f9cf282
skhila <bachana.skhiladze.1@btu.edu.ge>
2023-08-08 16:52:07 +0400

    Feature: Added Generified classes and methods

--- src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
+++ src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
@@ -1,50 +1,33 @@
 package ge.tbc.testautomation.javaoop.runners;
 
-import ge.tbc.testautomation.annotationsAndComparators.Analyzable;
-import ge.tbc.testautomation.annotationsAndComparators.RectangleComparator;
-import ge.tbc.testautomation.annotationsAndComparators.VariableNameAnnotation;
-import ge.tbc.testautomation.annotationsAndComparators.Rectangle;
-
+import ge.tbc.testautomation.generics.AnyPair;
+import ge.tbc.testautomation.generics.FigurePair;
+import ge.tbc.testautomation.javaoop.figures.Circle;
+import ge.tbc.testautomation.javaoop.figures.Rectangle;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
-@SuppressWarnings("unused")
 public class Main {
     public static void main(String[] args){
-//        Task #6
-        System.out.println("** Check if Annotation names equal field names in Analyzable class **");
-        Field[] analyzableFields = Analyzable.class.getDeclaredFields();
-        String fieldAnnotationName;
-        for (Field field : analyzableFields){
-            fieldAnnotationName = field.getAnnotation(VariableNameAnnotation.class).name();
-            if (!fieldAnnotationName.equalsIgnoreCase(field.getName())){
-                System.out.println("VARIABLE NAME MISMATCH FOUND");
-            }
-        }
 
-//        Task #7
-        int unusedInt = 8;
-        boolean unusedBoolean = false;
-        String unusedString = "Hello, I'm Unused String";
+        Integer integerForAnyPair = 11;
+        String stringForAnyPair = "String";
+        AnyPair<Field[], Field[]> declaredFields = getDeclaredFields(integerForAnyPair, stringForAnyPair);
+        System.out.println("** Fields of Integer class **");
+        System.out.println(Arrays.asList(declaredFields.getElementOne()));
+        System.out.println("** Fields of String class **");
+        System.out.println(Arrays.asList(declaredFields.getElementTwo()));
 
-//        Task #8
-        System.out.println("** Sorting Integer ArrayList **");
-        ArrayList<Integer> integerArrayList = new ArrayList<>();
-        Collections.addAll(integerArrayList, 11, 22, 33, 5, 2, 6, 7, 1, 3, 4);
-        Collections.sort(integerArrayList);
-        System.out.println("Sorted Integer Arraylist: " + integerArrayList);
+        Circle circle = new Circle(11);
+        Rectangle rectangle = new Rectangle(11, 121);
+        FigurePair<Circle, Rectangle> circleRectangleFigurePair = new FigurePair<>(circle, rectangle);
+        System.out.println("** FigurePair of Circle and Rectangle **");
+        System.out.println(circleRectangleFigurePair);
+    }
+    public static <K, D> AnyPair<Field[], Field[]> getDeclaredFields(K objOne, D objTwo){
+        Field[] fieldsOfObjOne = objOne.getClass().getDeclaredFields();
+        Field[] fieldsOfObjTwo = objTwo.getClass().getDeclaredFields();
 
-//        Task #13-14
-        System.out.println("** Reverse sorting Rectangle List using custom comparator **");
-        List<Rectangle> rectangleList = new ArrayList<>();
-        Collections.addAll(rectangleList, new Rectangle(1, 2), new Rectangle(3, 4), new Rectangle(5, 6),
-                                          new Rectangle(7, 8), new Rectangle(9, 10), new Rectangle(11, 12),
-                                          new Rectangle(13,14), new Rectangle(15, 16), new Rectangle(17, 18),
-                                          new Rectangle(19,20));
-        RectangleComparator rectangleComparator = new RectangleComparator();
-        rectangleList.sort(rectangleComparator);
-        System.out.println("Reverse sorted Rectangle List: " + rectangleList);
+        return new AnyPair<>(fieldsOfObjOne, fieldsOfObjTwo);
     }
 }
